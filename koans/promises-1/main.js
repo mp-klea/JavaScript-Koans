c355e82c1eb81787d5f5231edad5ec02be15c96a
Klea <klea.kolaric@makepositive.com>
2016-09-21 15:37:36 +0100

    local temp solution

--- koans/promises-1/main.js
+++ koans/promises-1/main.js
@@ -13,12 +13,27 @@ const SAMURAIPRINCIPLE = {};
       return fetchJson('data/leaderboard.json');
     };
   };
+  SAMURAIPRINCIPLE.BetterLeaderboardService = function () {
+    this.getLeaderboard = function () {
+      return fetchJson('data/leaderboard.json');
+    };
+  };
 }());
 
 let playerService = new SAMURAIPRINCIPLE.PlayerService(),
   leaderboardService = new SAMURAIPRINCIPLE.LeaderboardService();
+  betterLeaderboardService = new SAMURAIPRINCIPLE.BetterLeaderboardService();
+
 
 playerService.getPlayer(1)
   .then(player => console.log('Player', player), reason => console.log(reason));
 leaderboardService.getLeaderboard()
   .then(leaderboard => console.log('Leaderboard', leaderboard), reason => console.log(reason));
+betterLeaderboardService.getLeaderboard()
+  .then(function(leaderboard) {
+    var nameArray = [];
+    for(var i=0; i<leaderboard.length; i++) {
+      playerService.getPlayer(leaderboard[i].id).then(player => nameArray.push(player.name));
+    }
+    console.log(nameArray);
+  }, reason => console.log(reason));
